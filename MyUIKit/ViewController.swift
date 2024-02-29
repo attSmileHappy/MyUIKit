75cd9f8023b0e224e08cee1282ff57a9740a8d26
yezi <allthetimesh29@gmail.com>
2024-03-01 02:12:15 +0900

    01_Commit_2024.03.01 1. UIKit StroyBoard View & ViewController
    by.allthetimesh29

--- MyUIKit/ViewController.swift
+++ MyUIKit/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLable: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad() // 스토리보드의 있는 요소들을 다 그렸다는 호출 함수
+        view.backgroundColor = .blue //SwiftUI의 Color와는 타입이 다름 "UIColor"임
     }
 
-
+    @IBAction func touchMyButton(_ sender: Any) {
+        myLable.text = "I Love You <3"
+    }
+    
 }
 
