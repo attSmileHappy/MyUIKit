38961f23eb189ad5997058625bd664bf44f1805f
yezi <allthetimesh29@gmail.com>
2024-03-04 03:06:54 +0900

    06_Commit_2024.03.04 1. UITableViewDelegate & UITableViewDataSource func
    by.allthetimesh29

--- MyUIKit/MyTableViewController.swift
+++ MyUIKit/MyTableViewController.swift
@@ -39,4 +39,29 @@ extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = cellData[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexpath: IndexPath) -> CGFloat {
+        if indexpath.row == 0 {
+            return 40
+        } else {
+            return 20
+        }
+    }
+    /*
+     01. UITableViewDataSource : 어떤 데이터로 어떻게 그려질 것인지와 관련된 매소드들이 많음.
+     - func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+     상위 두개의 펑션은 필수로 구현해줘야 함. 한 섹션에 몇개로 그릴 것인지 어떤 셀로 그릴 것인지 지정해줘여함.
+     만약 두개의 펑션에 대한 구현이 없을 시 기본 값으로 1이 들어감. 즉, 위아래 모두 섹션 없이 한개로 나옴
+     - DataSource 쪽은 테이블뷰를 그리는 것에 관심이 더 많음.
+     함수 이름은 같지만 파라미터가 다른데, 해당 파라미터를 통해 구현 가능.
+     - 나중에 정의된 함수들을 시간내서 한번 쭉 보는게 좋을 것 같음.
+     
+     02. UITableViewDelegate : dataSource 쪽보다는 Action과 관련된게 더 많음
+     - Ex.     optional func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
+     : 셀이 보여지기 바로 직전에 다운로드 받아서 표시해야하는 이미지에 대한 액션
+     
+     03. Tip
+     - 함수의 파라미터 이름을 지을 때 애플 스타일로 지으면 협업이 편리해짐.
+     */
 }
