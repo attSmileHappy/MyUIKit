62ec13f6ca3d5c9f28fddd7a8cb18de0fb05db46
yezi <allthetimesh29@gmail.com>
2024-03-06 12:45:08 +0900

    10_Commit_2024.03.06 1. Segue
    by.allthetimesh29

---
+++ MyUIKit/BlueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BlueViewController.swift
+//  MyUIKit
+//
+//  Created by 김예지 on 3/6/24.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
